bcb853b99f76ba400f1f89f7627a0e1368c91d32
MOHAMED A D GBLA <mohamedadgbla2000@gmail.com>
2022-10-02 09:33:24 +0000

    this is a new commit

---
+++ hombo.js
@@ -0,0 +1,162 @@
+// var a = 20;
+// if(a === 20){
+//     console.log("The answernis true")
+// }
+
+
+// if(a === 10){
+//     console.log("The answernis true")
+// }
+// else{
+//     console.log("GO AND DO MORE RESEARCH");
+
+// }
+
+
+// var age =18;
+// if (age < 18){
+//     console.log("you cannot vote");
+// }
+// else if(age===18){
+//     crossOriginIsolated.log("you can vote");
+// }
+// else if(age > 18){
+//     crossOriginIsolated.log("you are elajable to vote");
+// }
+
+{/* <h4>loop control statement</h4> */}
+
+// for(var i =0; i<=50; i++  ){
+//     console.log(i);
+
+// }
+
+{/* <h5>while loop</h5> */}
+// var i =0;
+// while(i<=11){
+//     console.log(i);
+//     i++;
+// // }
+// var i =0;
+// while(i<=11){
+//     console.log(i);
+//     i=i+2;
+// // }
+
+{/* <h6> DO WHILE LOOP</h6> */}
+
+// var i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// } while(i>=40);
+
+{/* <h3> arrays</h3> */}
+
+// var a  = [10,3,7,6,8,5]
+// console.log(a)
+
+// var a=[10,3,7,6,8,5]
+// for(i++; i<a.length; i++){
+//     console.log(a[i]);
+// }
+
+// var a = [10,3,7,6,8,5]
+// for(var i = 0; i < a.length; i++){
+//     if(i % 2 ===0){
+//         console.log(a[i])
+//     }
+// }
+
+{/* <h5>average</h5> */}
+
+// var array = [10,20,30,40,50,]
+// var mao = array.length
+// var sum = 0;
+// for(var i = 0; i < mao; i++){
+//     sum +=array[i]
+// }
+// var average = sum / mao
+// console.log("the average is: "+average)
+
+// function
+
+// function addition(){
+//     var a= 10;
+//     var b = 20;
+//     var c = a+b;
+//     console.log("the value of c is "+c)
+// }SSS
+// addition()
+
+// function addition(a,b){
+//     var c = a + b 
+//     console.log("the value of c is "+c)
+// }
+// addition()
+
+// function pressed(){
+//     var text = document.getElementById("name").value
+//     if(text === "red"){
+//         document.getElementById("header").style.color = "red"
+//     }
+//     else if (text === "green"){
+//         document.getElementById("header").style.color = "green"
+
+//     }
+//     else if (text === "yellow"){
+//         document.getElementById("header").style.color = "yellow"
+
+//     }
+
+//     else if (text === "pink"){
+//         document.getElementById("header").style.color = "pink"
+
+//     }
+
+//     else if (text === "brown"){
+//         document.getElementById("header").style.color = "brown"
+
+//     }
+
+//     else if (text === "violet"){
+//         document.getElementById("header").style.color = "violet"
+
+//     }
+
+
+// }
+
+// let a = 5;
+// let t = 22;
+// let f = 11;
+//  m = a + t;
+//  p = m + f;
+
+//  var a = 44;
+//  var b = 82;
+//  var c = a + b ;
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
